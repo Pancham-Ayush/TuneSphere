f74f1db2281244742fb71afe0b4a6718087b1239
Pancham-Ayush <panchamayush@gmail.com>
2026-06-17 00:37:14 +0530

    temp

--- APIGateWay/src/main/java/com/example/apigateway/FallBack/FallbackController.java
+++ APIGateWay/src/main/java/com/example/apigateway/FallBack/FallbackController.java
@@ -2,7 +2,6 @@ package com.example.apigateway.FallBack;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,27 +12,32 @@ import java.util.Map;
 @RequestMapping("/fallback")
 class FallbackController {
 
-    @GetMapping("/player")
+    @RequestMapping("/player")
     public ResponseEntity<Map<String, Object>> playerFallback() {
         return buildResponse("PLAYER-SERVICE", "Player service is down");
     }
 
-    @GetMapping("/search")
+    @RequestMapping("/security")
+    public ResponseEntity<Map<String, Object>> securityrFallback() {
+        return buildResponse("SECURITY-SERVICE", "Security service is down");
+    }
+
+    @RequestMapping("/search")
     public ResponseEntity<Map<String, Object>> searchFallback() {
         return buildResponse("SEARCH-SERVICE", "Search service is down");
     }
 
-    @GetMapping("/ai")
+    @RequestMapping("/ai")
     public ResponseEntity<Map<String, Object>> aiFallback() {
         return buildResponse("AI-SERVICE", "AI service is down");
     }
 
-    @GetMapping("/yt")
+    @RequestMapping("/yt")
     public ResponseEntity<Map<String, Object>> ytFallback() {
         return buildResponse("YT-SERVICE", "YT service is down");
     }
 
-    @GetMapping("/s3")
+    @RequestMapping("/s3")
     public ResponseEntity<Map<String, Object>> s3Fallback() {
         return buildResponse("S3-SERVICE", "S3 service SSE down");
     }
